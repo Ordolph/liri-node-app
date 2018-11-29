f7bbb217b282485467af4decd18f5ab3cb0ed02c
Ordolph <austin.t.randolph@gmail.com>
2018-11-28 21:16:53 -0500

    Added functional inquirer prompts to get user input

--- liri.js
+++ liri.js
@@ -2,78 +2,115 @@ require("dotenv").config();
 require("./keys.js");
 
 var fs = require("fs");
-var axios = require("axios")
-var moment = require("moment")
+var axios = require("axios");
+var moment = require("moment");
 var Spotify = require('node-spotify-api');
+var inquirer = require("inquirer");
 
+let doIt = 0
 
-const command = process.argv[2];
-const inputParam = process.argv[3]
+const choicesArray = ["Get Concerts.", "Get Song.", "Get Movie.", "Do what the txt file says."]
 
-let concertThis = function(artist) {
-    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+let concertThis = function () {
+    inquirer.prompt([{
+        type: "input",
+        message: "What artist would you like to search for?",
+        name: "artist"
+    }]).then(result => {
+        var artist = result.artist.replace(/\s/g, "+");
 
-    axios({
-        method: 'get',
-        url: url,
-        responseType: 'json'
-    }).then(function (response) {
-        console.log("Here is your result" + "\n" +
-            "Location: " + response.data[0].venue.name + ", " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country + "." + "\n" +
-            "Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"))
-    });
-}
-let spotifyThis = function(song) {
+        if (artist === undefined || artist === "") {
+            artist = "BrunoMars"
+        }
 
-    if (song === undefined) {
-        song = "The Sign ace of base"
-    }
+        var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-    var spotify = new Spotify({
-        id: process.env.SPOTIFY_ID,
-        secret: process.env.SPOTIFY_SECRET,
-    });
-
-    spotify.search({
-        type: "track",
-        query: song,
-    }).then(function (response) {
-        console.log("Here is your result." + "\n" +
-            "Artist(s): " + response.tracks.items[0].artists[0].name + "\n" +
-            "Song: " + response.tracks.items[0].name + "\n" +
-            "Link: " + response.tracks.items[0].preview_url + "\n" +
-            "Album: " + response.tracks.items[0].album.name)
+        axios({
+            method: 'get',
+            url: url,
+            responseType: 'json'
+        }).then(function (response) {
+            console.log("Here is your result" + "\n" +
+                "Location: " + response.data[0].venue.name + ", " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country + "." + "\n" +
+                "Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"))
+        });
     })
 
 }
+let spotifyThis = function () {
 
-let movieThis = function(movie) {
-    var url = "http://www.omdbapi.com/?apikey=a3f03ecf&t=" + movie;
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the name of your song?",
+        name: "song",
+    }]).then(result => {
 
-    if (movie === undefined) {
-        console.log("If you haven't watched 'Mr.Nobody' then you should: http://www.imdb.com/title/tt0485947/" + "\nIt's on Netflix!")
-    }
+        var song = result.song;
 
-    else {
-        axios({
-            method: "get",
-            url: url,
-            responseType: "json"
+        if (song === undefined || song === "") {
+            song = "The Sign ace of base"
+        }
+
+        var spotify = new Spotify({
+            id: process.env.SPOTIFY_ID,
+            secret: process.env.SPOTIFY_SECRET,
+        });
+
+        spotify.search({
+            type: "track",
+            query: song,
         }).then(function (response) {
             console.log("Here is your result." + "\n" +
-                "Title: " + response.data.Title + "\n" +
-                "Year: " + response.data.Year + "\n" +
-                "IMDB Rating: " + response.data.Ratings[0].Value + "\n" +
-                "Rotten Tomatoes Score: " + response.data.Ratings[1].Value + "\n" +
-                "Country: " + response.data.Country + "\n" +
-                "Language: " + response.data.Language + "\n" +
-                "Plot Synopsis: " + response.data.Plot + "\n" +
-                "Actors: " + response.data.Actors)
+                "Artist(s): " + response.tracks.items[0].artists[0].name + "\n" +
+                "Song: " + response.tracks.items[0].name + "\n" +
+                "Link: " + response.tracks.items[0].preview_url + "\n" +
+                "Album: " + response.tracks.items[0].album.name)
         })
-    }
+    })
+
+
 }
 
-let doWhatItSays = function() {
+let movieThis = function () {
+    inquirer.prompt([{
+        type: "input",
+        message: "What movie would you like to search for?",
+        name: "movie"
+    }]).then(result => {
+        var movie = result.movie.replace(/\s/g, "+");
+        console.log(movie)
+
+        var url = "http://www.omdbapi.com/?apikey=a3f03ecf&t=" + movie;
+
+        if (movie === undefined || movie === "") {
+            console.log("If you haven't watched 'Mr.Nobody' then you should: http://www.imdb.com/title/tt0485947/" + "\nIt's on Netflix!")
+        }
+
+        else {
+            axios({
+                method: "get",
+                url: url,
+                responseType: "json"
+            }).then(function (response) {
+                console.log("Here is your result." + "\n" +
+                    "Title: " + response.data.Title + "\n" +
+                    "Year: " + response.data.Year + "\n" +
+                    "IMDB Rating: " + response.data.Ratings[0].Value + "\n" +
+                    "Rotten Tomatoes Score: " + response.data.Ratings[1].Value + "\n" +
+                    "Country: " + response.data.Country + "\n" +
+                    "Language: " + response.data.Language + "\n" +
+                    "Plot Synopsis: " + response.data.Plot + "\n" +
+                    "Actors: " + response.data.Actors)
+            })
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+
+
+}
+
+let doWhatItSays = function () {
     fs.readFile("./random.txt", "utf8", function (err, data) {
         console.log("Doing what it says.")
 
@@ -98,20 +135,30 @@ let doWhatItSays = function() {
     })
 }
 
-switch (command) {
-    case "concert-this":
-        concertThis(inputParam);
-        break;
 
-    case "spotify-this-song":
-        spotifyThis(inputParam)
-        break;
-
-    case "movie-this":
-        movieThis(inputParam);
-        break;
-
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
+inquirer.prompt([{
+    type: "list",
+    message: "What would you like to do?",
+    choices: choicesArray,
+    name: "command",
 }
+]).then(result => {
+    console.log(result.command)
+    switch (result.command) {
+        case "Get Concerts.":
+            concertThis();
+            break;
+
+        case "Get Song.":
+            spotifyThis()
+            break;
+
+        case "Get Movie.":
+            movieThis()
+            break;
+
+        case "Do what the txt file says.":
+            doWhatItSays();
+    }
+})
+
